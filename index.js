0aec3f2b5cc2a009c5c4a223ab62bd47877be66b
TharHtet236401 <tharhteta3@gmail.com>
2024-08-20 18:39:44 +0700

    before adding express handle bars

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import { dirname } from "path";
 import { logger } from "./middlewares/logger.js";
 import membersRouter from "./routes/api/members.js";
 
+
 dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
@@ -15,10 +16,17 @@ const __dirname = dirname(__filename);
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(logger);
 app.use(express.static(path.join(__dirname, "public"))); // Serve static files from the 'public' directory
 
 app.use("/api/members", membersRouter);
+
+
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  res.status(500).send("Internal Server Error");
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
--- routes/api/members.js
+++ routes/api/members.js
@@ -1,5 +1,6 @@
 import express from "express";
 import users from "../../Members.js";
+import { v4 as uuidv4 } from "uuid";
 import { logger } from "../../middlewares/logger.js";
 const router = express.Router();
 
@@ -7,13 +8,71 @@ router.get("/", (req, res) => {
   res.send(users);
 });
 
-router.get("/:userId", (req, res) => {
-  const user = users.filter((user) => user.id === parseInt(req.params.userId));
+router.post("/", (req, res) => {
+
+  if (!req.body.name || !req.body.email) {
+    return res.status(400).json({ message: "Name and email are required" });
+  }
+  const user = {
+    id: uuidv4(),
+    name: req.body.name,
+    email: req.body.email,
+    status: 'active'
+  };
+  try {
+    users.push(user);
+  } catch (error) {
+    console.error("Error pushing user:", error);
+    return res.status(500).send("Internal Server Error");
+  }
   res.send(user);
 });
 
-router.get("/*", (req, res) => {
-  res.status(404).send("<h1>404 Not Found</h1>");
+router.put("/:userId", (req, res) => {
+  const user = users.filter((user) => user.id === parseInt(req.params.userId));
+  if (user.length === 0) {
+    res
+      .status(404)
+      .json({ message: `User with id ${req.params.userId} not found` });
+  }
+  const updatedUser = {
+    id: uuidv4(),
+    name: req.body.name,
+    email: req.body.email,
+    status: 'active'
+  };
+  users.forEach((user) => {
+    if (user.id === parseInt(req.params.userId)) {
+      user.name = updatedUser.name? updatedUser.name : user.name;
+      user.email = updatedUser.email? updatedUser.email : user.email;
+      user.status = updatedUser.status? updatedUser.status : user.status;
+
+      res.send(user);
+    }
+   
+  });
+
+  
 });
 
+router.get("/:userId", (req, res) => {
+  const user = users.filter((user) => user.id === parseInt(req.params.userId));
+  if (user.length === 0) {
+    res
+      .status(404)
+      .json({ message: `User with id ${req.params.userId} not found` });
+  }
+  res.send(user);
+});
+
+router.delete("/:userId", (req, res) => {
+  const index = users.findIndex((user) => user.id === parseInt(req.params.userId));
+  
+  if (index === -1) {
+    return res.status(404).json({ message: `User with id ${req.params.userId} not found` });
+  }
+  
+  const deletedUser = users.splice(index, 1); // Remove the user by index
+  res.send(deletedUser); // Send the deleted user as a response
+});
 export default router;
