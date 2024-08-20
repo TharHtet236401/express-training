e066ae7194bdedb861e10ca49a39be3ee0657f17
TharHtet236401 <tharhteta3@gmail.com>
2024-08-20 15:43:46 +0700

    added with the middleware known as logger.js

---
+++ Members.js
@@ -0,0 +1,124 @@
+const users = [
+    {
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      status: 'active'
+    },
+    {
+      id: 2,
+      name: 'Jane Doe',
+      email: 'janedoe@example.com',
+      status: 'inactive'
+    },
+    {
+      id: 3,
+      name: 'Emily Smith',
+      email: 'emily.smith@example.com',
+      status: 'active'
+    },
+    {
+      id: 4,
+      name: 'Michael Brown',
+      email: 'michael.brown@example.com',
+      status: 'inactive'
+    },
+    {
+      id: 5,
+      name: 'Sarah Johnson',
+      email: 'sarah.johnson@example.com',
+      status: 'active'
+    },
+    {
+      id: 6,
+      name: 'David Lee',
+      email: 'david.lee@example.com',
+      status: 'inactive'
+    },
+    {
+      id: 7,
+      name: 'Olivia Martin',
+      email: 'olivia.martin@example.com',
+      status: 'active'
+    },
+    {
+      id: 8,
+      name: 'William Davis',
+      email: 'william.davis@example.com',
+      status: 'inactive'
+    },
+    {
+      id: 9,
+      name: 'Ava Garcia',
+      email: 'ava.garcia@example.com',
+      status: 'active'
+    },
+    {
+      id: 10,
+      name: 'James Wilson',
+      email: 'james.wilson@example.com',
+      status: 'inactive'
+    },
+    {
+      id: 11,
+      name: 'Isabella Rodriguez',
+      email: 'isabella.rodriguez@example.com',
+      status: 'active'
+    },
+    {
+      id: 12,
+      name: 'Benjamin Thompson',
+      email: 'benjamin.thompson@example.com',
+      status: 'inactive'
+    },
+    {
+      id: 13,
+      name: 'Mia Sanchez',
+      email: 'mia.sanchez@example.com',
+      status: 'active'
+    },
+    {
+      id: 14,
+      name: 'Alexander White',
+      email: 'alexander.white@example.com',
+      status: 'inactive'
+    },
+    {
+      id: 15,
+      name: 'Charlotte Harris',
+      email: 'charlotte.harris@example.com',
+      status: 'active'
+    },
+    {
+      id: 16,
+      name: 'Ethan Walker',
+      email: 'ethan.walker@example.com',
+      status: 'inactive'
+    },
+    {
+      id: 17,
+      name: 'Abigail Russell',
+      email: 'abigail.russell@example.com',
+      status: 'active'
+    },
+    {
+      id: 18,
+      name: 'Liam Brooks',
+      email: 'liam.brooks@example.com',
+      status: 'inactive'
+    },
+    {
+      id: 19,
+      name: 'Sophia Jenkins',
+      email: 'sophia.jenkins@example.com',
+      status: 'active'
+    },
+    {
+      id: 20,
+      name: 'Noah Hall',
+      email: 'noah.hall@example.com',
+      status: 'inactive'
+    }
+  ];
+
+  export default users;
--- index.js
+++ index.js
@@ -3,144 +3,23 @@ import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import users from "./Members.js";
+
+import { logger } from "./middlewares/logger.js";
 
 dotenv.config();
-
-const users = [
-  {
-    id: 1,
-    name: 'John Doe',
-    email: 'johndoe@example.com',
-    status: 'active'
-  },
-  {
-    id: 2,
-    name: 'Jane Doe',
-    email: 'janedoe@example.com',
-    status: 'inactive'
-  },
-  {
-    id: 3,
-    name: 'Emily Smith',
-    email: 'emily.smith@example.com',
-    status: 'active'
-  },
-  {
-    id: 4,
-    name: 'Michael Brown',
-    email: 'michael.brown@example.com',
-    status: 'inactive'
-  },
-  {
-    id: 5,
-    name: 'Sarah Johnson',
-    email: 'sarah.johnson@example.com',
-    status: 'active'
-  },
-  {
-    id: 6,
-    name: 'David Lee',
-    email: 'david.lee@example.com',
-    status: 'inactive'
-  },
-  {
-    id: 7,
-    name: 'Olivia Martin',
-    email: 'olivia.martin@example.com',
-    status: 'active'
-  },
-  {
-    id: 8,
-    name: 'William Davis',
-    email: 'william.davis@example.com',
-    status: 'inactive'
-  },
-  {
-    id: 9,
-    name: 'Ava Garcia',
-    email: 'ava.garcia@example.com',
-    status: 'active'
-  },
-  {
-    id: 10,
-    name: 'James Wilson',
-    email: 'james.wilson@example.com',
-    status: 'inactive'
-  },
-  {
-    id: 11,
-    name: 'Isabella Rodriguez',
-    email: 'isabella.rodriguez@example.com',
-    status: 'active'
-  },
-  {
-    id: 12,
-    name: 'Benjamin Thompson',
-    email: 'benjamin.thompson@example.com',
-    status: 'inactive'
-  },
-  {
-    id: 13,
-    name: 'Mia Sanchez',
-    email: 'mia.sanchez@example.com',
-    status: 'active'
-  },
-  {
-    id: 14,
-    name: 'Alexander White',
-    email: 'alexander.white@example.com',
-    status: 'inactive'
-  },
-  {
-    id: 15,
-    name: 'Charlotte Harris',
-    email: 'charlotte.harris@example.com',
-    status: 'active'
-  },
-  {
-    id: 16,
-    name: 'Ethan Walker',
-    email: 'ethan.walker@example.com',
-    status: 'inactive'
-  },
-  {
-    id: 17,
-    name: 'Abigail Russell',
-    email: 'abigail.russell@example.com',
-    status: 'active'
-  },
-  {
-    id: 18,
-    name: 'Liam Brooks',
-    email: 'liam.brooks@example.com',
-    status: 'inactive'
-  },
-  {
-    id: 19,
-    name: 'Sophia Jenkins',
-    email: 'sophia.jenkins@example.com',
-    status: 'active'
-  },
-  {
-    id: 20,
-    name: 'Noah Hall',
-    email: 'noah.hall@example.com',
-    status: 'inactive'
-  }
-];
-
-
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
+
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from the 'public' directory
+app.use(logger);
+app.use(express.static(path.join(__dirname, "public"))); // Serve static files from the 'public' directory
 
 app.get("/api/users", (req, res) => {
-
-     res.send(users);
+  res.send(users);
 });
 
 app.get("/*", (req, res) => {
